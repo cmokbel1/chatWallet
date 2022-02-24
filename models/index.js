66687a71072b89f89cd696ab8653ac5662c98575
Buff4eyes <claudemokbel@gmail.com>
2022-02-23 17:27:37 -0800

    typo

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const User = require('./User.js')
 const Room = require('./room.js')
 
-User.hasMany(Post, { foreignKey: 'uid' })
+User.hasMany(Room, { foreignKey: 'uid' })
 Room.belongsTo(User, { foreignKey: 'uid' })
 
 module.exports = { User, Room }
